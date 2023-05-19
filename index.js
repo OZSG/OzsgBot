270915d5fdca935b440c966c573c8cb7d8e6858d
Melih Özübek <melihozubek.19@protonmail.com>
2023-05-19 22:15:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
 const { Client, Collection, Events, GatewayIntentBits,Intents, ApplicationCommandOptionType } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+client.on(Events.ClientReady, c => {
+	console.log(`Ready! sss in as ${c.user.tag}`);
+	const GuildId = '781272820272463912'
+	const guild = client.guilds.cache.get(GuildId);
+	let commands
+	if (guild) {
+		commands = guild.commands;
+	} else {
+		commands = client.application?.commands;
+	}
+	commands?.create({
+		name: 'ping',
+		description: 'Botun ping değerini gösterir.',
+	})
+	commands?.create({
+		name: 'server',
+		description: 'Sunucu bilgilerini gösterir.',
+	})
+	commands?.create({
+		name: 'user',
+		description: 'Kullanıcı bilgilerini gösterir.',
+	})
+	commands?.create({
+		name: 'adayolustur',
+		description: 'Aday olmak için başvuru yapar.',
+		options: [
+			{
+				name: 'isim',
+				description: 'İsminizi giriniz.',
+				required: true,
+				type: 3
+			},
+			{
+				name: 'soyisim',
+				description: 'Soyisminizi giriniz.',
+				type: 3,
+				required: true,
+			},
+			{
+				name: 'yas',
+				description: 'Yaşınızı giriniz.',
+				type: 4,
+				required: true,
+			}
+		]
+	})
+
+});
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
