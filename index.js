55a19665b1e72b116ad2dbb3e41f7dc25605577b
Melih Özübek <melihozubek.19@protonmail.com>
2023-05-29 23:59:45 +0300

    d

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Client, Events, GatewayIntentBits, ApplicationCommandOptionType, Embed, EmbedBuilder } = require('discord.js');
-//const { token } = require('./config.json');
-//const {dbtoken} = require('./config.json');
+const { token } = require('./config.json');
+const {dbtoken} = require('./config.json');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 let botbilgi = [];
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const clientt = new MongoClient(process.env.MTOKEN, {
+const clientt = new MongoClient(dbtoken, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -21,6 +21,7 @@ const db= true;
 		await clientt.connect();
 		await clientt.db("OzsgSecim").command({ ping: 1 });
 		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+		// BOT BİLGİLERİ ALINIYOR..
 		const collectionName = "Bot";
 		const database = clientt.db("OzsgSecim");
 		const collection = database.collection(collectionName);
@@ -43,7 +44,7 @@ const db= true;
 			botbilgi.push(doc.Bilgiler.value);
 		}).then(() => {
 			console.log("Connected successfully to server");
-			client.login(process.env.DTOKEN);
+			client.login(token);
 		});
 		//console.log(botbilgi[0]);
 		
@@ -68,10 +69,10 @@ client.on(Events.ClientReady, c => {
 	} else {
 		commands = client.application?.commands;
 	}
-	commands?.create({
-		name: 'secimolustur',
-		description: 'Secim olusturur.',
-	})
+	// commands?.create({
+	// 	name: 'secimolustur',
+	// 	description: 'Secim olusturur.',
+	// })
 	commands?.create({
 		name: 'secmenolustur',
 		description: 'Secim Secmenini olusturur.',
@@ -82,6 +83,18 @@ client.on(Events.ClientReady, c => {
 				type: 6,
 				required: true,
 			},
+		]
+	})
+	commands?.create({
+		name: 'oyver',
+		description: 'Oy verir.',
+		options: [
+			{
+				name: 'aday',
+				description: 'Aday Seçiniz',
+				type: 8,
+				required: true,
+			}
 		]
 	})
 	commands?.create({
@@ -127,7 +140,7 @@ client.on(Events.ClientReady, c => {
 			{
 				name: 'no',
 				description: 'Aday numarasini giriniz.',
-				type: 4,
+				type: 8,
 				required: true,
 			},
 			{
@@ -158,22 +171,179 @@ client.on(Events.ClientReady, c => {
 		name: 'ayarlistele',
 		description: 'Aday listesini gösterir.',
 	})
+	commands?.create({
+		name: 'secimigoruntule',
+		description: 'Secim sonuclarini gösterir.',
+	})
+	commands?.create({
+		name: 'secimolustur',
+		description: 'Secim olusturur.',
+	})
 });
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isCommand()) return;
 	//console.log(interaction);
 	const { commandName } = interaction;
+	let secmencommand = ["oyver","secimigoruntule"];
+	let admincommand = ["veritabanikur", "adayayarla", "adaylistele", "ayarlistele", "secmenolustur", "secimolustur"];
+	if (admincommand.includes(commandName)) {
+		if (!interaction.member.roles.cache.has(botbilgi[1])) {
+			return interaction.reply({ content: 'Bu komutu kullanmak için yeterli yetkiye sahip değilsin.', ephemeral: true });
+		}
+	}else if (secmencommand.includes(commandName)) {
+		//KOMUTU KULLANAN SEÇMEN LİSTESİNDE Mİ ?
+		const dbNames = "OzsgSecim";
+		const collectionNames = "Secmenler";
+		const databases = clientt.db(dbNames);
+		const collections = databases.collection(collectionNames);
+		const query = { name: interaction.user.id };
+		const result = await collections.findOne(query);
+		if (!result) {
+			return interaction.reply({ content: 'Bu komutu kullanmak için yeterli yetkiye sahip değilsin.', ephemeral: true });
+		}else{
+			switch (commandName) {
+				case "oyver":
+					console.log("secmen oy veriyor..");
+			const rolid = interaction.options.getRole('aday').id;
+			// adayi arastir oy vermis mi vermemis mi
+			const dbNames = "OzsgSecim";
+			const collectionNames = "Secmenler";
+			const databases = clientt.db(dbNames);
+			const collections = databases.collection(collectionNames);
+			const query = { name: interaction.user.id };
+			const result = await collections.findOne(query);
+			if (result) {
+				console.log("secmen bulundu");
+				// oy no yu kontrol et
+				const oy = result.Bilgiler.oy;
+				if (oy == 0) {
+					console.log("oy verilmemis");
+					// rolid Aday listesinde var mi
+					const dbNamesx = "OzsgSecim";
+					const collectionNamesx = "Adaylar";
+					const databasesx = clientt.db(dbNamesx);
+					const collectionsx = databasesx.collection(collectionNamesx);
+					const query = { no: rolid };
+					const result = await collectionsx.findOne(query);
+					if (result) {
+						console.log("aday bulundu");
+						// oy sayısını arttır
+						result.Bilgiler.oysayisi = result.Bilgiler.oysayisi + 1;
+						const newvalues = { $set: { Bilgiler: result.Bilgiler } };
+						collectionsx.updateOne(query, newvalues, function (err, res) {
+							if (err) throw err;
+							console.log("1 document updated");
+						});
+					}else{
+							console.log("aday bulunamadi");
+							return interaction.reply({ content: 'Aday bulunamadı.', ephemeral: true });
+						}
+					
+					// Secmenin verdiği oyu güncelle
+					const dbNames2 = "OzsgSecim";
+					const collectionNames2 = "Secmenler";
+					const databases2 = clientt.db(dbNames2);
+					const collections2 = databases2.collection(collectionNames2);
+					const query2 = { name: interaction.user.id };
+					const result2 = await collections2.findOne(query2);
+					if (result2) {
+						result2.Bilgiler.oy = rolid;
+					const newvalues2 = { $set: { Bilgiler: result2.Bilgiler } };
+					collections2.updateOne(query2, newvalues2, function (err, res) {
+						if (err) throw err;
+						console.log("1 document updated");
+					});
+					}	
+					interaction.reply({ content: 'Oyunuz alındı.', ephemeral: true });				
+					// oy verilmemis
+				}else{
+					console.log("oy verilmis");
+					// oy verilmis
+					return interaction.reply({ content: 'Zaten oy vermişsin.', ephemeral: true });
+				}
+			}
+					break;
+				case "secimigoruntule":
+					//TAMAMLANDI
+					console.log("secim sonuclari gosteriliyor..");
+					const dbxname = "OzsgSecim";
+					const dbxcollection = "Adaylar";
+					const dbx = clientt.db(dbxname);
+					const dbxc = dbx.collection(dbxcollection);
+					const queryx = { };
+					const resultx = await dbxc.find(queryx).toArray();
+					if (resultx) {
+						console.log("adaylar bulundu");
+						//console.log(resultx);
+						let adaylar = "";
+						// id to role name in guild id
+						//const guildid = client.guilds.cache.get(botbilgi[0]);
+						//const guild = client.guilds.cache.get(botbilgi[0]);
+						for (let index = 0; index < resultx.length; index++) {
+							//const role = guild.roles.cache.get(resultx[index].no);
+							//console.log(role.name);
+							adaylar = adaylar + resultx[index].Bilgiler.parti + " : " + resultx[index].Bilgiler.oysayisi + "\n";
+					}
+						console.log(adaylar);
+						const embed = new EmbedBuilder()
+						.setColor('#0099ff')
+						.setTitle('Seçim Sonuçları')
+						.setDescription(String(adaylar))
+						.setTimestamp();
+						//.setFooter('OzsgBot');
+						interaction.reply({ embeds: [embed] });
+					}else{
+						console.log("adaylar bulunamadi");
+						return interaction.reply({ content: 'Adaylar bulunamadı.', ephemeral: true });
+					}
+					break;
+			
+				default:
+					break;
+			}
+			// TAMAMLANDI
+		}
+	}
+
 	switch (commandName) {
 		case 'secmenolustur':
+			// TAMAMLANDI
 			const isimid = interaction.options.getUser('isim').id;
 			const isim = interaction.options.getUser('isim').username;
-			console.log(isim + ' ' + isimid);
+			//console.log(isim + ' ' + isimid);
+			const dbNames = "OzsgSecim";
+			const collectionNames = "Secmenler";
+			const databases = clientt.db(dbNames);
+			const collections = databases.collection(collectionNames);
+			// create array
+			const adaybilgi = [
+				{
+				name: isimid,
+				Bilgiler: {
+					isim: isim,
+					oy: 0,
+				}}
+			]
+			//secmenin varlığını kontrol et
+			const queryy = { name: isimid };
+			const result = await collections.findOne(queryy);
+			if (result) {
+				console.log(`A document was found with the _id: ${result._id}`);
+				return interaction.reply({ content: 'Bu secmen zaten var.', ephemeral: true });
+			}
+			try {
+				const result = await collections.insertMany(adaybilgi);
+				console.log(`A document was inserted with the _id: ${result.insertedId}`);
+			} catch (err) {
+				console.log(`Failed to insert a document: ${err}`);
+			}
 			// const guild = client.guilds.cache.get(interaction.guildId);
 			// console.log(guild);
 			interaction.reply('Secmen olusturuldu.');
 			break;
 		case 'veritabanikur':
+			// TAMAMLANDI
 			const dbName = "OzsgSecim";
 			const collectionName = "Bot";
 			const database = clientt.db(dbName);
@@ -209,27 +379,24 @@ client.on(Events.InteractionCreate, async interaction => {
 			interaction.reply({ content: 'Veritabanı kuruldu.', ephemeral: true });
 			break;
 		case 'adayayarla':
-			// check server admin perm
-			if (!interaction.member.permissions.has('ADMINISTRATOR')) {
-				interaction.reply({ content: 'Bu komutu kullanmak için yeterli yetkiye sahip değilsin.', ephemeral: true });
-				return;
-			}
+			// check server admin permission
 
 			const collectionName2 = "Adaylar";
 			const database2 = clientt.db("OzsgSecim");
 			const collection2 = database2.collection(collectionName2);
 			const Aday = [
 				{
-					no: interaction.options.getInteger('no'),
+					no: interaction.options.getRole('no').id,
 					Bilgiler: {
 						isim: interaction.options.getUser('isim').id,
 						ikincilisim: interaction.options.getUser('ikincilisim').id,
-						parti: interaction.options.getString('parti')
+						parti: interaction.options.getString('parti'),
+						oysayisi: 0,
 					},
 				}
 			]
-			if (await collection2.findOne({ no: interaction.options.getInteger('no') })) {
-				const findOneQuery = { no: interaction.options.getInteger('no') };
+			if (await collection2.findOne({ no: interaction.options.getRole('no').id })) {
+				const findOneQuery = { no: interaction.options.getRole('no').id };
 				const UpdateDocument = { $set: { Bilgiler: { isim: interaction.options.getUser('isim').id, ikincilisim: interaction.options.getUser('ikincilisim').id, parti: interaction.options.getString('parti') } } };
 				const updateOptions = { returnOriginal: false };
 				await collection2.findOneAndUpdate(findOneQuery, UpdateDocument, updateOptions);
